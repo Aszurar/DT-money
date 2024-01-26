5530dca4f63cb55850ee4bdbf74191beef079ced
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-26 14:33:18 -0300

    ✨ Add Loading component ✨

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from 'react'
+
+type LoadingProps = ComponentProps<'div'>
+
+export function Loading({ className, ...props }: LoadingProps) {
+  return (
+    <div
+      className={`border-top-4-table h-5 w-5 animate-spin rounded-full 
+        border-4 border-white 
+        ${className}`}
+      {...props}
+    />
+  )
+}
