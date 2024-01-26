7356c7f85fcbbf60a91f92de27ff57286ad79d26
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-26 14:30:54 -0300

    💄 Update Logo component styling 💄

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -1,10 +1,10 @@
-import LogoSVG from '../../../../public/logo.svg'
+import LogoSVG from '../../../assets/logo.svg'
 
 export function Logo() {
   return (
-    <div className="flex gap-4 items-center">
+    <div className="flex items-center gap-4">
       <img src={LogoSVG} alt="" className="h-10" />
-      <h1 className="text-gray-100  text-title font-bold">DT Money</h1>
+      <h1 className="text-title  font-bold text-gray-100">DT Money</h1>
     </div>
   )
 }
