cd6eeb73fb02b4665862f13428c1529f26ca6fa7
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-26 14:32:47 -0300

    :recycle: Refactor Button component styles :recycle:

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,30 +1,24 @@
-import { ComponentProps } from "react"
-import { VariantProps, tv } from "tailwind-variants"
-
+import { ComponentProps } from 'react'
+import { VariantProps, tv } from 'tailwind-variants'
 
 const button = tv({
-  base: `text-white font-bold px-5 py-3 cursor-pointer transition-all 
-  duration-300 bg-green-500 rounded-md `,
+  base: `flex text-white font-bold px-5 py-3 cursor-pointer transition-all 
+  duration-300 bg-green-500 rounded-md hover:bg-green-300  justify-center
+  disabled:cursor-not-allowed disabled:opacity-60 items-center
+  disabled:hover:bg-green-500 
+  `,
   variants: {
     variant: {
-      solid: '',
-      outline: `flex items-center gap-3 px-8 py-3.5 bg-transparent border 
-      border-green-300 text-green-300 font-bold 
-      hover:text-white hover:bg-green-300`
-    }
+      outline: `flex  gap-3 px-8 py-3.5 bg-gray-900 border 
+      border-green-300 text-green-300 font-bold h-fit
+      hover:text-white 
+      disabled:hover:bg-gray-900 disabled:hover:opacity-60`,
+    },
   },
-  defaultVariants: {
-    variant: 'solid'
-  }
 })
 
-type ButtonProps = ComponentProps<"button"> & VariantProps<typeof button>
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
 
-export function Button({variant, ...props}: ButtonProps) {
-  return (
-    <button 
-    className={button({variant})} 
-    {...props}
-    />
-  )
+export function Button({ variant, className, ...props }: ButtonProps) {
+  return <button className={button({ variant, className })} {...props} />
 }
