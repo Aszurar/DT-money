53535b101d76ea136ccf0bc386b500d0706cae94
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-26 14:33:07 -0300

    ✨ Add new transaction modal to header ✨

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { Logo } from './Logo'
 import { Button } from '../Button'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from './NewTransactionModal'
 
 export function Header() {
   return (
-    <header className="bg-gray-900 h-header ">
-      <div className="max-w-app m-auto flex justify-between pt-10 items-center">
+    <header className="h-header bg-gray-900 ">
+      <div className="m-auto flex max-w-app items-center justify-between px-6 pt-10">
         <Logo />
-        <Button>Nova transação</Button>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <Button>Nova transação</Button>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </div>
     </header>
   )
